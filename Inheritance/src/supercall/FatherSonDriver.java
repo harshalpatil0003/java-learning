cfb738475fedd4192b553292e4e835fc11588e2f
Phobial <harshalpatilcsn2020@gmail.com>
2025-06-14 10:30:25 +0530

    Update FatherSonDriver.java

--- Inheritance/src/supercall/FatherSonDriver.java
+++ Inheritance/src/supercall/FatherSonDriver.java
@@ -4,8 +4,15 @@ public class FatherSonDriver {
 
 	public static void main(String[] args) {
 		Son s1=new Son();
-		s1.displayName();
+		Father f1= new Father();
 		System.out.println(s1.name);
 		s1.displayFatherSonDetails();
+		
+		// ✅instanceof operator is used to check 
+		// that all the members of classes are in object. 
+		System.out.println(f1 instanceof Son);
+		System.out.println(f1 instanceof Father);
+		System.out.println(s1 instanceof Son);
+		System.out.println(s1 instanceof Father);
 	} 
 } 
