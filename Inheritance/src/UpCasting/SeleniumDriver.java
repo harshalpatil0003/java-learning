662e8de162351c5f79504ff44bd67d77b3a9bacb
Phobial <harshalpatilcsn2020@gmail.com>
2025-06-15 19:41:51 +0530

    added upcasting and downcasting

--- Inheritance/src/UpCasting/SeleniumDriver.java
+++ Inheritance/src/UpCasting/SeleniumDriver.java
@@ -3,5 +3,14 @@ package UpCasting;
 public class SeleniumDriver {
 public static void main(String[] args) {
 	
-}
+	// ChromeDriver is upcasted to RemoteDriver.
+	RemoteDriver r1=new ChromeDriver();
+	// RemoteDriver is upcasted to WebDriver.
+	WebDriver w1 =new RemoteDriver();
+	// RemoteDriver is downcasted to chromeDriver
+	ChromeDriver c1=(ChromeDriver)r1;
+	c1.webDriver();
+	c1.remoteDriver();
+	c1.chromeDriver();
+	}
 }
