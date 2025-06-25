bdc55a17b100bc078c4e325d24378762af442a76
Phobial <harshalpatilcsn2020@gmail.com>
2025-06-26 00:45:52 +0530

    Story and message method is added

--- Interfaces/src/Instagram/InstaDriver.java
+++ Interfaces/src/Instagram/InstaDriver.java
@@ -3,10 +3,13 @@ package Instagram;
 public class InstaDriver {
 
 	public static void main(String[] args) {
-		Insta i1=new InstaImpl1();
+		Object
+		Insta i1=new InstaImpl2();
 		i1.post("Too Good To Be True! ❤️❤️", "harshal");
 		
 		i1.like("rohit");
 		i1.comment("aditya", "true 🙌🙌");
+		i1.story("Feeling Worm 😶‍🌫️😶‍🌫️", "harsh");
+		i1.message("hello!", "jay");
 	}
 }
--- Interfaces/src/Instagram/InstaImpl2.java
+++ Interfaces/src/Instagram/InstaImpl2.java
@@ -1,5 +1,15 @@
 package Instagram;
 
-public class InstaImpl2 {
+public class InstaImpl2 extends InstaImpl1{
 
+	public void story(String story,String user) {
+		System.out.println(story);
+		System.out.println(user+" Posted a story 📷");
+		System.out.println("------------------------");
+	}
+	public void message(String msg,String name) {
+		System.out.println(msg);
+		System.out.println(name+" send a message ✍️");
+		System.out.println("------------------------");
+	}
 }
