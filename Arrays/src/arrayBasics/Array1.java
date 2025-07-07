077f0564a69855c3730d06517302270eb7617041
Phobial <harshalpatilcsn2020@gmail.com>
2025-07-07 21:23:43 +0530

    array created

---
+++ Arrays/src/arrayBasics/Array1.java
@@ -0,0 +1,20 @@
+package arrayBasics;
+
+public class Array1{
+	public static void main(String[] args) {
+		
+	//array reference //array object
+		     int[] arr=new int[5];
+		     // prints reference of array object
+		     System.out.println(arr);
+		
+		     String[] arr2= new String[3];
+		     System.out.println(arr2);
+		
+		     double[] arr3= new double[5];
+		     System.out.println(arr3);
+		
+		     boolean[] arr4= new boolean[2];
+		     System.out.println(arr4);
+	}
+}
