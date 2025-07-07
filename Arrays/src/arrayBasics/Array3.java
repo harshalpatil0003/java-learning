3d94592f4e88b873c31d1dca5c20e33b76153567
Phobial <harshalpatilcsn2020@gmail.com>
2025-07-07 23:22:23 +0530

    another way of creating array

---
+++ Arrays/src/arrayBasics/Array3.java
@@ -0,0 +1,15 @@
+package arrayBasics;
+
+public class Array3 {
+	public static void main(String[] args) {
+		
+		//another way of creating arrays
+		int[] arr= {2,4,6,8,10,12,14,16,18,20};
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+
+}
