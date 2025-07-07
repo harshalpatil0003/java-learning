b688d70df3c371bf025bff19cd52e13f4f3c35af
Phobial <harshalpatilcsn2020@gmail.com>
2025-07-07 21:38:26 +0530

    prints elements in array

--- Arrays/src/arrayBasics/Array1.java
+++ Arrays/src/arrayBasics/Array1.java
@@ -3,18 +3,36 @@ package arrayBasics;
 public class Array1{
 	public static void main(String[] args) {
 		
-	//array reference //array object
+	// array reference //array object
 		     int[] arr=new int[5];
 		     // prints reference of array object
 		     System.out.println(arr);
+		     
+		     // prints the elements of array
+		     System.out.println(arr[0]);
+		     System.out.println(arr[1]);
+		     System.out.println(arr[2]);
+		     System.out.println(arr[3]);
+		     System.out.println(arr[4]);
 		
 		     String[] arr2= new String[3];
 		     System.out.println(arr2);
+		     
+		     System.out.println(arr2[0]);
+		     System.out.println(arr2[1]);
+		     System.out.println(arr2[2]);
 		
-		     double[] arr3= new double[5];
+		     double[] arr3= new double[2];
 		     System.out.println(arr3);
-		
+		     
+		     System.out.println(arr3[0]);
+		     System.out.println(arr3[1]);
+		     
+		     
 		     boolean[] arr4= new boolean[2];
 		     System.out.println(arr4);
+		     
+		     System.out.println(arr4[0]);
+		     System.out.println(arr4[1]);
 	}
 }
