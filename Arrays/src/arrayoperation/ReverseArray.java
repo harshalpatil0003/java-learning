0bf548a44522382e49a697038fc74c02482bb18a
Phobial <harshalpatilcsn2020@gmail.com>
2025-07-10 11:22:23 +0530

    array reverse

--- Arrays/src/arrayoperation/ReverseArray.java
+++ Arrays/src/arrayoperation/ReverseArray.java
@@ -1,18 +1,22 @@
 package arrayoperation;
 
+import java.util.Arrays;
+
 public class ReverseArray{
 	
 	public static void main(String[] args) {
 		
 		int[] arr= {2,4,6,8,10};
-		reverse(arr);
-		System.out.println();
+		System.out.println(Arrays.toString(reverse(arr)));
 	}
 	
-	public static void reverse(int[] arr) {
+	public static int[] reverse(int[] arr) {
 		
+		int[] ans=new int[arr.length];
 		for (int i = arr.length-1; i >=0; i--) {
+			//ans[i]=(arr[i]);
 			System.out.println(arr[i]);
 		}
+		return ans;
 	}
 }
