4e8edd81761a99e18059a82eb289ba907c50e366
Phobial <harshalpatilcsn2020@gmail.com>
2025-07-12 22:56:10 +0530

    program to add element at last

---
+++ Arrays/src/Assignment5/AddElementAtLast.java
@@ -0,0 +1,32 @@
+package Assignment5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AddElementAtLast {
+
+	public static void main(String[] args) {
+		int[] arr= {2,4,6,8};
+		
+		System.out.print("Enter Element to add: ");
+		int ele= new Scanner(System.in).nextInt();
+		System.out.println(Arrays.toString(addElementToArray(arr,ele)));
+		//addElementToArray(arr,ele);
+		
+	}
+	public static int[] addElementToArray(int[] arr, int ele) {
+		
+		int[] ans= new int[arr.length+1];
+		
+		for (int i = 0; i < ans.length; i++) {
+			if (i<arr.length) {
+				ans[i]=arr[i];
+				//System.out.println(i);
+			}
+			else if (i>=arr.length) {
+				ans[i]=ele;
+			} 
+		}
+		return ans;
+	}
+}
