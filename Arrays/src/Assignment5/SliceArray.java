81441a35db72dcdbbc927f8ab71a0b22bba4e146
Phobial <harshalpatilcsn2020@gmail.com>
2025-07-15 10:11:28 +0530

    a new method is designed to slice an array from its start and end point

--- Arrays/src/Assignment5/SliceArray.java
+++ Arrays/src/Assignment5/SliceArray.java
@@ -8,8 +8,11 @@ public class SliceArray {
 	public static void main(String[] args) {
 		int[] arr= {10,20,30,40,50,60,70};
 		System.out.println(Arrays.toString(sliceArray(arr)));
+		System.out.println(Arrays.toString(sliceArray2(arr, 2, 5,false)));
 		
 	}
+	
+	// removing custom elements from array
 	public static int[] sliceArray(int[] arr) {
 		int count=0;
 		for (int i = 0; i < arr.length; i++) {
@@ -29,5 +32,38 @@ public class SliceArray {
 		i++;
 		}
 		return ans;
+	}
+	
+	// removing an array from another array
+	public static int[] sliceArray2(int[]arr, int start, int end) {
+		
+		if (start>0 && end<=arr.length && start<end) {
+			int[] ans= new int[arr.length-(end-start)];
+			
+			for (int i = 0; i < arr.length;) {
+				if (i<start) {
+					ans[i]=arr[i];
+					i++;
+				}
+				else if (i==start) {
+					i+=(end-start);
+					
+				}
+				else if(i>=end) {
+					ans[i-(end-start)]=arr[i];
+					i++;
+				}
+			}
+			return ans;
+		}
+		return null;
+	}
+	
+	public static int[] sliceArray2(int[] arr ,int start, int end, boolean inc) {
+		if (inc) {
+			return sliceArray2(arr, start, end+1);
+		}
+		else
+			return sliceArray2(arr, start, end);
 	}
 }
