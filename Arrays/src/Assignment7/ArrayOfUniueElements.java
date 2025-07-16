e24483f55f2cc3daa293d58e62bdbdeda280fdd4
Phobial <harshalpatilcsn2020@gmail.com>
2025-07-16 21:30:18 +0530

    Update ArrayOfUniueElements.java

--- Arrays/src/Assignment7/ArrayOfUniueElements.java
+++ Arrays/src/Assignment7/ArrayOfUniueElements.java
@@ -6,11 +6,40 @@ import java.util.Iterator;
 public class ArrayOfUniueElements {
 
 	public static void main(String[] args) {
-		int[] arr= {1,2,3,2,1,5,6};
+		int[] arr= {1,2,3,2,1,5,6,3};
+		
+		System.out.println(Arrays.toString(frequency(arr)));
+		
 		System.out.println(Arrays.toString(uniqueElements(arr)));
 		
+		System.out.println(Arrays.toString(singleElements(arr)));
+		
+		System.out.println(Arrays.toString(repeated(arr)));
+		
 	}
+	// method that finds the frequency of array elements.
+		public static int[] frequency(int[] arr) {
+			int[] freq= new int[arr.length];
+			
+			for (int i = 0; i < arr.length; i++) 
+			{
+				int cnt=0;
+				if (freq[i]!=-1) {
+					
+					for (int j = i+1; j < freq.length; j++) {
+						if (arr[j]==arr[i]) {
+							cnt++;
+							freq[j]=-1;
+						}
+					}
+					freq[i]=cnt;
+				}
+			}
+			return freq;
+		}
+		
 	// methods return array of unique elements
+	//output: [1,2,3,5,6]
 	public static int[] uniqueElements(int[] arr) {
 		int[] freq=frequency(arr);
 		int ct=0;
@@ -34,24 +63,53 @@ public class ArrayOfUniueElements {
 		
 	}
 	
-	// method that finds the frequency of array elements.
-	public static int[] frequency(int[] arr) {
-		int[] freq= new int[arr.length];
-		
-		for (int i = 0; i < arr.length; i++) 
-		{
-			int cnt=0;
-			if (freq[i]!=-1) {
-				
-				for (int j = i+1; j < freq.length; j++) {
-					if (arr[j]==arr[i]) {
-						cnt++;
-						freq[j]=-1;
-					}
-				}
-				freq[i]=cnt;
+	
+	
+	// array of those elements who didn't repeat once
+	//output: [5,6]
+	public static int[] singleElements(int[] arr) {
+		int[] freq= frequency(arr);
+		int ct=0;
+		for (int i = 0; i < freq.length; i++) {
+			if (freq[i]==0) {
+				ct++;
 			}
 		}
-		return freq;
+		
+		int[] ans= new int[ct];
+		if (ct>0) {
+			for (int i = 0,j=0; i < freq.length; i++) {
+				if (freq[i]==0) {
+					ans[j]=arr[i];
+					j++;
+				}
+			}
+		}
+		return ans;
+	}
+	
+	//array of repeated elements
+	//output: [1,2,3]
+	public static int[] repeated(int[] arr) {
+		
+		int[] freq= frequency(arr);
+		int ct=0;
+		for (int i = 0; i < freq.length; i++) {
+			if (freq[i]==1) {
+				ct++;
+			}
+		}
+		int[] ans=new int[ct];
+		if (ct>0) 
+		{
+			for (int i = 0,j=0; i < freq.length; i++) {
+				if (freq[i]==1) {
+					ans[j]=arr[i];
+					j++;
+				}
+			}
+			
+		}
+		return ans;
 	}
  }
