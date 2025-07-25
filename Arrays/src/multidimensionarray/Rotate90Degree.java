f5904741735189698b67dabbdd44688c7ab412c1
Harshal Patil <117168808+harshalpatil0003@users.noreply.github.com>
2025-07-26 00:43:22 +0530

    rotate array 90 degree

--- Arrays/src/multidimensionarray/Rotate90Degree.java
+++ Arrays/src/multidimensionarray/Rotate90Degree.java
@@ -1,37 +1,37 @@
 package multidimensionarray;
 
 import java.util.Arrays;
-import java.util.Iterator;
 
 public class Rotate90Degree {
 
 	public static void main(String[] args) {
-		int[][] arr= {	{1,2,3},
-						{4,5,6},
-						{7,8,9}};
+		char[][] arr= {	{'a','b','c','d'},
+						{'e','f','g','h'},
+						{'i','j','k','l'},
+						{'m','n','o','p'}};
 		rotate(arr);
 	}
 	
-	public static void rotate(int[][] arr) {
+	public static void rotate(char[][] arr) {
 		
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
 				if (j>i) {
-					int temp=arr[i][j];
+					char temp=arr[i][j];
 					arr[i][j]=arr[j][i];
 					arr[j][i]=temp;
 				}
 			}
 		}
-		for (int i = 0,j=arr.length-1; i < arr.length; i++) {
-			for (int j2 = 0,k=arr.length-1; j2 < arr[i].length; j2++) {
-				int[] temp=arr[j2];
-				arr[j2]=arr[k];
-				arr[k]=temp;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0,k=arr[i].length-1; j < arr.length/2; j++) {
+				
+				char temp=arr[i][j];
+				arr[i][j]=arr[i][k];
+				arr[i][k]=temp;
 				k--;
-				System.out.println(Arrays.toString(arr[i]));
 			}
+			System.out.println(Arrays.toString(arr[i]));
 		}
-		
 	}
 }
