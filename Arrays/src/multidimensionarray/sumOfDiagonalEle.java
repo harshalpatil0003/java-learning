b9ff8dbbd8e0d2ef1915f141289b9d76ba4b7f5c
Harshal Patil <117168808+harshalpatil0003@users.noreply.github.com>
2025-07-26 12:13:03 +0530

    make diagonal elements zero

--- Arrays/src/multidimensionarray/sumOfDiagonalEle.java
+++ Arrays/src/multidimensionarray/sumOfDiagonalEle.java
@@ -1,5 +1,6 @@
 package multidimensionarray;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class sumOfDiagonalEle {
@@ -8,7 +9,8 @@ public class sumOfDiagonalEle {
 		int[][] arr= {	{1,2,3},
 						{4,5,6},
 						{7,8,9}	};
-		System.out.println(sumofDiagonalEle(arr));
+		System.out.println("Sum of diagonal elements: "+sumofDiagonalEle(arr));
+		makeDiagonalZero(arr);
 	}
 	public static int sumofDiagonalEle(int[][]arr) {
 		int sum=0;
@@ -21,5 +23,18 @@ public class sumOfDiagonalEle {
 			}
 		}
 		return sum;
+	}
+	public static void makeDiagonalZero(int[][] arr) {
+		System.out.println("Made diagonal elements zero");
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				if (i==j || i+j==arr.length-1) {
+					arr[i][j]=0;
+				}
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(Arrays.toString(arr[i]));
+		}
 	}
 }
