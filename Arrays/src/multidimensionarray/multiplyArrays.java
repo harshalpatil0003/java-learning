f5ae4f41a1e026aef4a3fc502724bc5fcd8d8cea
Harshal Patil <117168808+harshalpatil0003@users.noreply.github.com>
2025-07-30 22:50:19 +0530

    some modifications

--- Arrays/src/multidimensionarray/multiplyArrays.java
+++ Arrays/src/multidimensionarray/multiplyArrays.java
@@ -6,21 +6,23 @@ import java.util.Iterator;
 public class multiplyArrays {
 
 	public static void main(String[] args) {
-		int[][] arr1= {	{1,2},
-						{3,4}};
+		int[][] arr1= {	{1,2,3},
+						{2,3},
+						{3}};
 		
-		int[][] arr2= {{1,2},
-						{3,4}};
+		int[][] arr2= {{1,2,3},
+						{3},
+						{4}};
 		multiply(arr1, arr2);
 	}
 	public static void multiply(int[][]arr1, int[][] arr2) {
 		
-			if (arr1[0].length == arr2.length) {
+			if ( isSymmetric(arr1, arr2) && arr1.length == arr2[0].length) {
 				
 				int[][] ans= new int[arr1.length][arr2[0].length];
 				for (int i = 0; i < arr1.length; i++) {
 					for (int j= 0; j < arr2[0].length; j++) {
-						for (int k = 0; k < arr1[0].length; k++) {
+						for (int k = 0; k < arr2[0].length; k++) {
 							ans[i][j] += arr1[i][k] * arr2[k][j];
 						}
 					}
@@ -29,5 +31,25 @@ public class multiplyArrays {
 					System.out.println(Arrays.toString(ans[j]));
 				}
 			}
+			else {
+				System.out.println("Arrays are asymmetric ");
+			}
+	}
+	public static boolean isSymmetric(int [][] arr1, int[][] arr2) {
+		
+		boolean check1=true, check2=true;
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1.length!=arr1[0].length) {
+				check1= false;
+				break;
+			}
+		}
+		for (int i = 0; i < arr2.length; i++) {
+			if (arr2.length!=arr2[0].length) {
+				check2= false;
+				break;
+			}
+		}
+		return check1==check2;
 	}
 }
