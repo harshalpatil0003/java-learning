37fdda2dfa93b915b0c7cf2c9e11ecce006bebf2
Harshal Patil <117168808+harshalpatil0003@users.noreply.github.com>
2025-08-13 10:24:47 +0530

    check immutability of string

---
+++ Strings/src/StringBuiltInMethods/Immutability.java
@@ -0,0 +1,11 @@
+package StringBuiltInMethods;
+
+public class Immutability {
+
+	public static void main(String[] args) {
+		String s1="Hi";
+		s1=s1+" Hello";
+		//System.out.println(s1==s2);
+		System.out.println(s1);
+	}
+}
