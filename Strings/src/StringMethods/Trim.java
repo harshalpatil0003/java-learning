5c1ddc6eccf3afa08f5c8a8674ca13f93ac4a58e
Harshal Patil <117168808+harshalpatil0003@users.noreply.github.com>
2025-08-15 16:56:22 +0530

    trim method

---
+++ Strings/src/StringMethods/Trim.java
@@ -0,0 +1,26 @@
+package StringMethods;
+
+import java.util.Iterator;
+
+public class Trim {
+
+	public static void main(String[] args) {
+		String s="  Hars hal  ";
+		System.out.println(trim(s));
+	}
+	public static String trim(String s) {
+		String ans="";
+		for (int i = 0; i < s.length(); i++) {
+			
+			char ch=s.charAt(i);
+			if (ch==' ') {
+				continue;
+			}
+			else {
+				ans+=ch;
+			}
+		}
+		return ans;
+	}
+	
+}
