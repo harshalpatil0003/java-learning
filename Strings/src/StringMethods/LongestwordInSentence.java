03d9571fd2275e54464300966d9b9aec5a589867
Harshal Patil <117168808+harshalpatil0003@users.noreply.github.com>
2025-08-19 23:17:20 +0530

    longest word in sentence

---
+++ Strings/src/StringMethods/LongestwordInSentence.java
@@ -0,0 +1,23 @@
+package StringMethods;
+
+public class LongestwordInSentence {
+
+	public static void main(String[] args) {
+		String s="Iam like to learn encyclopedia";
+		System.out.println(longestString(s));
+	}
+	
+	public static String longestString(String s) {
+		
+		String[] sent=s.split(" ");
+		int max=0;
+		String ans="";
+		for (int i = 0; i < sent.length; i++) {
+			if (sent[i].length()>max) {
+				max=sent[i].length();
+				ans=sent[i];
+			}
+		}
+		return ans;
+	}
+}
