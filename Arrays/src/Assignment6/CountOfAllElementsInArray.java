d31dcc7855d0fb8a94286d2941ae2c66ff5cf8c9
Harshal Patil <117168808+harshalpatil0003@users.noreply.github.com>
2025-08-23 10:12:51 +0530

    Update CountOfAllElementsInArray.java

--- Arrays/src/Assignment6/CountOfAllElementsInArray.java
+++ Arrays/src/Assignment6/CountOfAllElementsInArray.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 public class CountOfAllElementsInArray {
 
 	public static void main(String[] args) {
-		int[] arr= {1,5,6,6,5,2,3,1,5,6};
+		int[] arr= {0,1,0,2,0,1,0};
 		System.out.println(Arrays.toString(countOccurances(arr)));
 		//countOccurances(arr);
 		
