58ba3a1ee0f69a39d8455f3e453915de2596624a
Harshal Patil <117168808+harshalpatil0003@users.noreply.github.com>
2025-10-02 18:09:15 +0530

    added to cart and removed from card

--- Automation Testing/com.crm.Selenium/src/HW4/AddToCart.java
+++ Automation Testing/com.crm.Selenium/src/HW4/AddToCart.java
@@ -12,18 +12,30 @@ public class AddToCart {
 		dr.manage().window().maximize();
 		dr.get("https://demowebshop.tricentis.com/");
 		dr.findElement(By.cssSelector("input[onclick=\"AjaxCart.addproducttocart_catalog('/addproducttocart/catalog/2/1/1    ');return false;\"]")).click();
+		Thread.sleep(2000);
+		dr.findElement(By.className("recipient-name")).sendKeys("Harshal");
+		Thread.sleep(1000);
+		dr.findElement(By.id("giftcard_2_RecipientEmail")).sendKeys("harshp123@gmail.com");
+		Thread.sleep(1000);
+		dr.findElement(By.id("giftcard_2_SenderName")).sendKeys("Harshal");
+		Thread.sleep(1000);
+		dr.findElement(By.id("giftcard_2_SenderEmail")).sendKeys("harshp123@gmail.com");
+		Thread.sleep(1000);
+		dr.findElement(By.id("giftcard_2_Message")).sendKeys("Jaldi Pohchao! ");
+		Thread.sleep(2000);
+		dr.findElement(By.id("addtocart_2_EnteredQuantity")).clear();
+		Thread.sleep(1000);
+		dr.findElement(By.id("addtocart_2_EnteredQuantity")).sendKeys("2");
+		Thread.sleep(1000);
+		dr.findElement(By.id("add-to-cart-button-2")).click();
+		Thread.sleep(1000);
+		dr.findElement(By.linkText("Shopping cart")).click();
+		dr.findElement(By.name("removefromcart")).click();
+		dr.findElement(By.cssSelector(".button-2.update-cart-button")).click();
 		
-	//	dr.findElement(By.cssSelector(".recipient-name")).sendKeys("harshal");
-		dr.findElement(By.id("add-to-wishlist-button-2")).click();
-		System.out.println("hi ganu");
-//		dr.findElement(By.id("giftcard_2_RecipientEmail")).sendKeys("harsh@gmail.com");
-//		dr.findElement(By.id("giftcard_2_SenderName")).sendKeys("Harshal");
-//		dr.findElement(By.id("giftcard_2_SenderEmail")).sendKeys("harshal@gmailcom");
-//		dr.findElement(By.id("giftcard_2_Message")).sendKeys("Jaldi Pohchao! ");
-		
-
-		
-//		Thread.sleep(2000);
+		Thread.sleep(3000);
 		dr.quit();
+		
+		
 	}
 }
