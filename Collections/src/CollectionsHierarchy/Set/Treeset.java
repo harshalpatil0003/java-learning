715bcb81e3756c7a491d31f621a34784746fc334
Harshal Patil <117168808+harshalpatil0003@users.noreply.github.com>
2025-10-07 18:55:04 +0530

    tried some different aproach

--- Collections/src/CollectionsHierarchy/Set/Treeset.java
+++ Collections/src/CollectionsHierarchy/Set/Treeset.java
@@ -7,43 +7,50 @@ import java.util.TreeSet;
 public class Treeset {
 
 	public static void main(String[] args) {
-		System.out.println("Enter sorting option: ");
-		 int opt= new Scanner(System.in).nextInt();
-		switch (opt) {
-		case 1: {
-			Comparator<FaceWash> cVol=(o1,o2)->o1.vol>o2.vol?1:o1.vol<o2.vol?-1:0 ;
-			TreeSet<FaceWash> f =new TreeSet<FaceWash>(cVol);
-			break;
-		}
-		case 2:{
-			Comparator<FaceWash> cPrice=(o1,o2)->o1.price>o2.price?1:o1.price<o2.price?-1:0;
-			TreeSet<FaceWash> f =new TreeSet<FaceWash>(cPrice);
-			break;
-		}
-		case 3:{
-			Comparator<FaceWash> cColor=(o1,o2)->o1.color.compareTo(o2.color);
-			TreeSet<FaceWash> f =new TreeSet<FaceWash>(cColor);
-			break;
-		} 
-		case 4: {
-			Comparator<FaceWash> cBrand=(o1,o2)->o1.brand.compareTo(o2.brand);
-			TreeSet<FaceWash> f =new TreeSet<FaceWash>(cBrand);
-			break;
-		}
-		default:
-			throw new IllegalArgumentException("Unexpected value: " + opt);
-		}	
-		TreeSet<FaceWash> f =new TreeSet<FaceWash>();
-		f.add(new FaceWash("Citaphil", 200, 100, "white"));
-		f.add(new FaceWash("Natures", 100 ,120, "yellow"));
-		f.add(new FaceWash("Garniar", 110, 200, "black"));
-		f.add(new FaceWash("Citaphil", 200.0, 100, "white"));
+			
+//		Comparator<FaceWash> cVol=(o1,o2)->o1.vol>o2.vol?1:o1.vol<o2.vol?-1:0 ;
+//		Comparator<FaceWash> cPrice=(o1,o2)->o1.price>o2.price?1:o1.price<o2.price?-1:0;
+//		Comparator<FaceWash> cColor=(o1,o2)->o1.color.compareTo(o2.color);
+//		Comparator<FaceWash> cBrand=(o1,o2)->o1.brand.compareTo(o2.brand);
+//		
+//		
+//		TreeSet<FaceWash> f= new TreeSet<FaceWash>();
+//		f.add(new FaceWash("Citaphil", 200, 100, "white"));
+//		f.add(new FaceWash("Natures", 100 ,120, "yellow"));
+//		f.add(new FaceWash("Garniar", 110, 200, "black"));
+//		f.add(new FaceWash("Citaphil", 200.0, 100, "white"));
+//		System.out.println(f);
+	
+// 		TreeSet<String > t= new TreeSet<String>();
+//		// Strings are Stored Lexicographically.
+// 		t.add("harshal");
+// 		t.add("ramesh");
+// 		t.add("patil");
+// 		t.add("andrew");
+// 		System.out.println(t);
 		
-		System.out.println(f);
+		TreeSet<Integer> b= new TreeSet<Integer>();	
+		b.add(23);
+		b.add(24);
+		b.add(3);
+		b.add(3);
+		b.add(2);
+		b.add(66);
+		b.add(96);
+		b.add(33);
+		b.add(32);
+		b.add(33);
+		
+		System.out.println(b);
+		System.out.println(b.reversed());
+		System.out.println(b.contains(66));
+		b.addLast(100);
+		
+		
 	}
 }
 
-class FaceWash{
+class FaceWash implements Comparable<FaceWash> {
 	 String brand;
 	 double price;
 	 int vol;
@@ -63,11 +70,10 @@ class FaceWash{
 		 return "["+brand+", "+price+", "+vol+", "+color+"]";
 	 }
 	 
-	// @Override
-//	public int compareTo(FaceWash o) {
-//		//return this.brand.compareTo(o.brand);
-//		 return (this.vol>o.vol)?1:(this.vol<o.vol?-1:0);
-//	}
+	 public int compareTo(FaceWash o) {
+		//return this.brand.compareTo(o.brand);
+		 return (this.vol>o.vol)?1:(this.vol<o.vol?-1:0);
+	}
 	
 
 }
